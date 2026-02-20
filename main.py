1dfc394a8bd87d53f3c47d616ac95dad0bd4b948
Kervenspn <kervenspn@live.com>
2026-02-20 02:28:57 -0500

    Converted code in main.py to a function.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """There are 3 errors in this code. Please use your own ability to find them all. Consider this a Python refresher."""
 def run_total():
-    total = 0
+    total= 0
     for i in range(5):
         number = int(input("Enter a number: "))
         total += number
